48428be1cf908c18f9d70bd689e22a9b9be1c33b
Kushagra Patidar <kushagra24ai027@satiengg.in>
2024-04-04 18:49:56 +0530

    v1.2

--- dowhile_flagged.py
+++ dowhile_flagged.py
@@ -2,13 +2,15 @@
 #Do Not Tinker
 flag=1
 
-#Initialization expression (Change as per need)
+#Initialization expression (Change 'i=10' as per need)
 i=10
 
 #Use the Flag variable to run 1st iteration of the while loop un-conditionally
 while flag or i<10:#Only change the Condition 'i<10' as per need. Do not tinker with the 'flag'
     #Body of the loop. Change as per need
     print("Hello World")
+    
+    # End of Body
     
     #Reset the Flag variable to 0 for condition based execution of the while loop
     #Do not tinker with below statement
